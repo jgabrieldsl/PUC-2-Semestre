6b6c2bde0e36011052df3dc945d8d67f16f80c62
jgabsx <joao.gsl@puccampinas.edu.br>
2024-09-23 07:47:54 -0300

    Struct ex 2

--- Matérias/Algoritmos e Linguagem de Programação - Prática/Aula 06 - Structs/Exercicio_2.cpp
+++ Matérias/Algoritmos e Linguagem de Programação - Prática/Aula 06 - Structs/Exercicio_2.cpp
@@ -10,15 +10,48 @@
 #include <iostream>
 using namespace std;
 
-#define N 10
+#define N 2
 
 struct Alunos {
     int RA;
     int cod_dic;
-    double nota1[];
-    double nota2[];
+    double nota_one;
+    double nota_two;
+    double media;
+};
+
+double calcular_media (double nota1, double nota2) {
+    double media_pond = ((nota1 * 1) + (nota2 * 2)) / 3;
+
+    return media_pond;
 }
+
+void mostrar_notas (Alunos alunos[]) {
+    for (int i = 0; i < N; i++) {
+        cout << "RA do " << i + 1 << "° aluno: "  << alunos[i].RA << endl;
+        cout << "Código da Disciplina: " << alunos[i].cod_dic << endl;
+        cout << "Nota 1: " << alunos[i].nota_one << endl;
+        cout << "Nota 2: " << alunos[i].nota_two << endl;
+        cout << "Média do " << i + 1 << "° aluno: " << alunos[i].media << endl << endl;
+    }
+}
+
 int main() {
+    Alunos alunos[N];
+
+    for (int i = 0; i < N; i++) {
+        cout << "Digite o RA do " << i + 1 << "° aluno: ";
+        cin >> alunos[i].RA;
+        cout << "Digite o código da disciplina: ";
+        cin >> alunos[i].cod_dic;
+        cout << "Digite a nota 1: ";
+        cin >> alunos[i].nota_one;
+        cout << "Digite a nota 2: ";
+        cin >> alunos[i].nota_two;
+        alunos[i].media = calcular_media(alunos[i].nota_one, alunos[i].nota_two);
+    }
+
+    mostrar_notas(alunos);
 
     return 0;
 }
