43b04afcd63752f7670c8e8cd0560b3bf0676fc8
jgabsx <joao.gsl@puccampinas.edu.br>
2024-09-26 10:39:36 -0300

    get test

--- Matérias/Introdução às Tecnologias para Web/projetos/super-backend/src/server.ts
+++ Matérias/Introdução às Tecnologias para Web/projetos/super-backend/src/server.ts
@@ -1,31 +1,20 @@
-import http from "http";
+// Importando o Express
+import express from 'express';
+import { Request, Response, Router } from 'express';
 
 const port = 3000;
+const server = express();
+const routes = Router();
 
-function calcularIMC (peso: number, altura: number) {
-    return (peso / (altura**2))
-}
+// Definindo rotas
 
-
-// Corpo (Header) & Conteúdo (Body)
-const server = http.createServer ((req, res) => {
-
-    if (req.url === "/calcCompCircunferencia") {
-        res.writeHead(200, {'content-type': 'text/html; charset=utf-8' });
-        res.end('<h1>Servico de cálculo do comprimento de uma cincunferência.</h1>');
-        
-    } else if (req.url === "/calcIMC") {
-        const IMC: number = calcularIMC(65, 1.86);
-        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        res.end(`<h1>Serviço de cálculo de IMC</h1> <p> Seu IMC é: ${IMC.toString()} </p>`);
-
-        
-    } else {
-        res.writeHead(404, { 'content-type': 'text/html; charset=utf-8' });
-        res.end('<h1>Serviço inexistente!</h1>')
-    }
+// Rota default 
+routes.post ('/', (req: Request, res: Response) => {
+    res.send ('Olá!');
 });
 
-server.listen ( port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+server.use(routes);
+
+server.listen (port, () => {
+    console.log(`Server is running on: ${port}`)
 });
