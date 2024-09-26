684a989b7871fe8c40e02230efbfcdc2a1223f32
jgabsx <joao.gsl@puccampinas.edu.br>
2024-09-26 09:32:54 -0300

    trabalho librarys g

---
+++ Matérias/Algoritmos e Linguagem de Programação - Prática/Aula 07 - Sistema de Gerenciamento de Biblioteca/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+struct livro {
+    char titulo[100];
+    char autor[100];
+    int num_paginas;
+    int ano_publicacao;
+    int id;
+    int quantidade_disponivel;
+    char nome_emprestaram[100];
+};
+
+struct pessoa {
+    char nome[100];
+    int documento;
+    int idade;
+
+};
+
+void PrintLivros(struct livro dados) {
+
+    cout << "Título: " << dados.titulo << endl;
+    cout << "Autor: " << dados.autor << endl;
+    cout << "Número de Páginas: " << dados.num_paginas << endl;
+    cout << "Id: " << dados.ano_publicacao << endl;
+    cout << "Quantidade de Exemplares Dísponiveis: " << dados.quantidade_disponivel << endl;
+    cout << "Nome das Pessoas que Emprestaram: " << dados.nome_emprestaram;
+
+}
+
+void cadastrar_livros(struct livro dados[], int *quant_cadastro) { 
+
+    cout << "Titulo: "; cin.getline(dados[*quant_cadastro].titulo, 100);
+
+    cout << "Autor : "; cin.getline(dados[*quant_cadastro].autor, 100);
+
+    cout << "Número de páginas: "; cin >> dados[*quant_cadastro].num_paginas;
+
+    cout << "Id: "; cin >> dados[*quant_cadastro].id;
+
+    cout << "Quantidade dísponivel: "; cin >> dados[*quant_cadastro].quantidade_disponivel;
+    
+    //cout << "Nome das pessoas que emprestaram: "
+}
+
+
+
+int main () {
+    struct livro vetLivros[100];
+    int qtd = 0;
+
+    while (true) {
+        int opt;
+        
+        do  {
+            cout << "/t Bem vindo ao Sistema de Gerenciamento de Biblioteca! /n /n";
+            cout << "1. Cadastro de livros" << endl;    
+            cout << "2. Consultar livros" << endl;    
+            cout << "3. Emprestar livro" << endl;    
+            cout << "4. Devolução de livros" << endl;    
+            cout << "5. Remoção de livros" << endl;   
+            cout << "6. Sair do programa;" << endl;
+            cin >> opt;
+            
+            switch (opt) {
+                case 1:
+                    cadastrar_livros (vetLivros, &qtd);    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    break;
+            }
+
+            cout << endl;
+        
+        } while (opt != 6);
+    }
+}
